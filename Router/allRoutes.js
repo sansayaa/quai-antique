8fc9aa9e97715ff2c7aa89c25278afa0d8e2061d
setteboun chainess <chaielie34@hotmail.com>
2024-07-28 12:29:25 +0200

    ajout des pages connexion+creation de compte+moncompte+modfiez le mots de passe

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "connexion", "/pages/signin.html"),
+    new Route("/signin", "connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
