75f15e3820632f4426f685eba8c3c7755a237dc3
setteboun chainess <chaielie34@hotmail.com>
2024-07-29 15:45:08 +0200

    Gestion de l'affichage réservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon Compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
-
+    new Route("/allResa", "Vos réservations", "/pages/réservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
